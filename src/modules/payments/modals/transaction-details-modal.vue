5f550d8249b997351236db2ab0ebc8a7dc344cbb
Aishat <aishat@vesicash.com>
2025-12-09 11:24:35 +0100

    Add charge and currency fields to transaction details modal.

--- src/modules/payments/modals/transaction-details-modal.vue
+++ src/modules/payments/modals/transaction-details-modal.vue
@@ -99,6 +99,8 @@ const props = defineProps<{ transaction: any }>();
 
 const displayFields = [
   { label: "Amount Paid", key: "amount" },
+  { label: "Charge", key: "charge" },
+  { label: "Currency", key: "currency" },
   { label: "Reference ID", key: "reference" },
   { label: "Date", key: "created_at" },
   { label: "Time", key: "created_at_time" },
@@ -107,10 +109,10 @@ const displayFields = [
   { label: "Email", key: "email" },
   { label: "MOMO Number", key: "phone_number" },
   { label: "Operator", key: "operator" },
-  { label: "Currency", key: "currency" },
   { label: "Business ID", key: "business_id" },
+  { label: "Redirect URL", key: "redirect_url" },
+  { label: "Environment", key: "environment" },
   { label: "Type", key: "type" },
-  { label: "Charge", key: "charge" },
   { label: "Attempts", key: "attempts" },
   { label: "Status", key: "status" },
 ];
@@ -152,9 +154,8 @@ const formatValue = (key: string) => {
     case "charge":
       return `${transactionData.value.currency}${formatNumber(transactionData.value.charge).toLocaleString()}`;
     default:
-      return transactionData.value[key].length > 0
-        ? transactionData.value[key]
-        : "—";
+     const value = transactionData.value[key];
+     return value || value === 0 ? value : "—";
   }
 };
 
